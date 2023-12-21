9e118a199de1a63b040947d074b9332d26c090a2
Jay Shah <rajs80266@gmail.com>
2023-12-21 11:14:39 -0800

    Other companies

--- index.js
+++ index.js
@@ -49,9 +49,17 @@ const getEightFoldCompanies = () => {
     return eightFoldCompanies;
 }
 
-const companies = [...getWorkdayCompanies(), ...getEightFoldCompanies()];
+const getOtherCompanies = () => {
+    const data = fs.readFileSync('otherCompanies.json', 'utf8');
+    return JSON.parse(data);
+}
+const companies = [
+    ...getWorkdayCompanies(),
+    ...getEightFoldCompanies(),
+    ...getOtherCompanies()
+];
 
-const getId = (json, path) => {
+const getValue = (json, path) => {
     let result = json;
     for (const key of path.split('.')) {
         result = result && result[key];
@@ -82,56 +90,110 @@ const addNewRoles = (newRows) => {
 };
 
 const run = async () => {
-    for (const company of companies) {
-        const response = await getResponse(company.link, company.request, company.name);
-        const newData = response[company.path];
+    try {
+        for (const company of companies.splice(0)) {
+            const response = await getResponse(company.link, company.request, company.name);
+            const newData = getValue(response, company.path);
 
-        const filePath = company.name + '.json';
-        let jsonData = [];
-        try {
-            const data = fs.readFileSync(filePath, 'utf8');
-            jsonData = JSON.parse(data);
-        } catch {}
-        const newRoles = newData.filter(newRole => !jsonData.some(oldRole =>
-            getId(oldRole, company.identifier) === getId(newRole, company.identifier)
-        ));
-        console.log(newRoles);
-        const today = new Date();
-        const updatedJson = [...jsonData, ...newRoles];
-        const updatedJsonData = JSON.stringify(updatedJson);
-        if (workdayCompanyNames.includes(company.name)) {
-            for(const newRole of newRoles) {
-                newRecords.push(
-                    {
-                        company: company.name,
-                        role: newRole.title.replace(new RegExp(',', 'g'), '-'),
-                        id: newRole.bulletFields[0],
-                        viewedAt: today.getDate() + '-' + today.getMonth() + '-' + today.getFullYear(),
-                        url: '-',
-                        notes: newRole.postedOn,
-                    }
-                )
+            const filePath = company.name + '.json';
+            let jsonData = [];
+            try {
+                const data = fs.readFileSync(filePath, 'utf8');
+                jsonData = JSON.parse(data);
+            } catch {}
+            const newRoles = newData.filter(newRole => !jsonData.some(oldRole =>
+                getValue(oldRole, company.identifier) === getValue(newRole, company.identifier)
+            ));
+            console.log(newRoles);
+            const today = new Date();
+            const updatedJson = [...jsonData, ...newRoles];
+            const updatedJsonData = JSON.stringify(updatedJson);
+            if (workdayCompanyNames.includes(company.name)) {
+                for(const newRole of newRoles) {
+                    newRecords.push(
+                        {
+                            company: company.name,
+                            role: newRole.title.replace(new RegExp(',', 'g'), '-'),
+                            id: newRole.bulletFields[0],
+                            viewedAt: today.getDate() + '-' + today.getMonth() + '-' + today.getFullYear(),
+                            url: '-',
+                            notes: newRole.postedOn,
+                        }
+                    )
+                }
+            } else if (eightFoldCompanyNames.includes(company.name)) {
+                for(const newRole of newRoles) {
+                    newRecords.push(
+                        {
+                            company: company.name,
+                            role: newRole.name.replace(new RegExp(',', 'g'), '-'),
+                            id: newRole.id,
+                            viewedAt: today.getDate() + '-' + today.getMonth() + '-' + today.getFullYear(),
+                            url: newRole.canonicalPositionUrl,
+                            notes: newRole.job_description,
+                        }
+                    )
+                }
+            } else if (company.name == "Amazon") {
+                for(const newRole of newRoles) {
+                    newRecords.push(
+                        {
+                            company: company.name,
+                            role: newRole.title.replace(new RegExp(',', 'g'), '-'),
+                            id: newRole.id_icims,
+                            viewedAt: today.getDate() + '-' + today.getMonth() + '-' + today.getFullYear(),
+                            url: "https://www.amazon.jobs" + newRole.job_path,
+                            notes: "Posted Date: " + newRole.posted_date + " Basic Qualification: " + newRole.basic_qualifications,
+                        }
+                    )
+                }
+            } else if (company.name == "Meta") {
+                for(const newRole of newRoles) {
+                    newRecords.push(
+                        {
+                            company: company.name,
+                            role: newRole.title.replace(new RegExp(',', 'g'), '-'),
+                            id: newRole.id,
+                            viewedAt: today.getDate() + '-' + today.getMonth() + '-' + today.getFullYear(),
+                            url: "https://www.metacareers.com/jobs/" + newRole.id,
+                            notes: "",
+                        }
+                    )
+                }
+            } else if (company.name == "Service Now") {
+                for(const newRole of newRoles) {
+                    newRecords.push(
+                        {
+                            company: company.name,
+                            role: newRole.data.title.replace(new RegExp(',', 'g'), '-'),
+                            id: newRole.data.req_id,
+                            viewedAt: today.getDate() + '-' + today.getMonth() + '-' + today.getFullYear(),
+                            url: newRole.data.apply_url,
+                            notes: "Posted Date: " + newRole.data.posted_date,
+                        }
+                    )
+                }
+            } else if (company.name == "Way Fair") {
+                for(const newRole of newRoles) {
+                    newRecords.push(
+                        {
+                            company: company.name,
+                            role: newRole.title.replace(new RegExp(',', 'g'), '-'),
+                            id: newRole.requisitionId,
+                            viewedAt: today.getDate() + '-' + today.getMonth() + '-' + today.getFullYear(),
+                            url: newRole.structuredDataApplyLink,
+                            notes: "Posted Date: " + newRole.createdDate,
+                        }
+                    )
+                }
             }
-        }
-        if (eightFoldCompanyNames.includes(company.name)) {
-            for(const newRole of newRoles) {
-                newRecords.push(
-                    {
-                        company: company.name,
-                        role: newRole.name.replace(new RegExp(',', 'g'), '-'),
-                        id: newRole.id,
-                        viewedAt: today.getDate() + '-' + today.getMonth() + '-' + today.getFullYear(),
-                        url: newRole.canonicalPositionUrl,
-                        notes: newRole.job_description,
-                    }
-                )
-            }
-        }
-        fs.writeFile(filePath, updatedJsonData, 'utf8', (err) => {
-            console.log('JSON file updated successfully.');
-        });
-    };
-
+            fs.writeFile(filePath, updatedJsonData, 'utf8', (err) => {
+                console.log('JSON file updated successfully.');
+            });
+        };
+    } catch(err) {
+        console.log(err);
+    }
     addNewRoles(newRecords);
 }
 
